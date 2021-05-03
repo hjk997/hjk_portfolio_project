a4edf925d33085011155b32ff3d5b70997f736eb
hjk997 <ununsk@gmail.com>
2021-05-03 20:06:08 +0900

    first commit
    새 프로젝트 생성 후 기존에 만든 html 소스와 합쳐서 정리

---
+++ src/test/java/com/portfolio/hjk/HjkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.hjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
