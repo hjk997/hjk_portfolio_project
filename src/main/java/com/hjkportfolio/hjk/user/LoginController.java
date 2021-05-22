149f5550bebf21f80fe2de64e1d087d4f8856eef
hjk997 <ununsk@gmail.com>
2021-05-23 03:54:19 +0900

    fragment html head, footer etc using thymeleaf replace tag
    html에서 head나 footer, nav 등 중복되는 코드를 replace를 사용해서 부분화하였습니다

--- src/main/java/com/hjkportfolio/hjk/user/LoginController.java
+++ src/main/java/com/hjkportfolio/hjk/user/LoginController.java
@@ -26,4 +26,59 @@ public class LoginController {
         model.addAttribute("data", "신기하다");
         return "test";
     }
+
+    @GetMapping("header")
+    public String header(){
+        return "header";
+    }
+
+    @GetMapping("footer")
+    public String footer(){
+        return "footer";
+    }
+
+    @GetMapping("nav")
+    public String nav(){
+        return "nav";
+    }
+
+    @GetMapping("contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("info")
+    public String info(){
+        return "info";
+    }
+
+    @GetMapping("project")
+    public String project(){
+        return "project";
+    }
+
+    @GetMapping("project-list")
+    public String projectList(){
+        return "project-list";
+    }
+
+    @GetMapping("tech-stack")
+    public String techStack(){
+        return "tech-stack";
+    }
+
+    @GetMapping("update-list")
+    public String updateList(){
+        return "update-list";
+    }
+
+    @GetMapping("write-project")
+    public String writeProject(){
+        return "write-project";
+    }
+
+    @GetMapping("write-update")
+    public String writeUpdate(){
+        return "write-update";
+    }
 }
