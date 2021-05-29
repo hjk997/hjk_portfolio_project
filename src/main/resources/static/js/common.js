c8820d4d5803f3c25c0b098cb6c0d68ce846b5b4
hjk997 <ununsk@gmail.com>
2021-05-30 03:57:08 +0900

    로그인 시 사용자 이름 텍스트 깜박임
    로그인을 하면 사용자의 이름이 visible/invisible 상태를 전환하며 깜박입니다.

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -11,12 +11,25 @@
     }
   }
 
-
 var login_text = document.getElementById("login_text");
 var user_span = document.getElementById("userName");
 if(user_span.innerHTML){
     user_span.innerHTML = user_span.innerHTML + "님 로그인 중";
     login_text.innerHTML = "로그아웃 하기";
+    setInterval(toggle, 700);
 }else{
     login_text.innerHTML = "관리자 로그인";
+}
+
+// javascript
+var shown = true;
+
+function toggle() {
+   if(shown) {
+       user_span.style.display = 'none';
+       shown = false;
+   } else {
+       user_span.style.display = '';
+       shown = true;
+   }
 }
