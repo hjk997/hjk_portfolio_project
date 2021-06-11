ce047de8fbcbb3e0d5dead51b102c1f1fb3ec3f5
hjk997 <ununsk@gmail.com>
2021-06-11 22:47:01 +0900

    업데이트 글 작성 시 제목, 내용 칸을 비우지 않도록 팝업창 띄우기
    글 작성 시 제목 란이나 내용 란이 비어있다면 내용을 입력하라는 팝업창을 띄웁니다.

---
+++ src/main/resources/static/js/write-update.js
@@ -0,0 +1,33 @@
+function show_modal(error_text){
+    var modal_text = document.getElementById("modal_error_text");
+    modal_text.innerHTML = error_text;
+    $("#failedLoginModal").modal();
+}
+
+
+// TODO : 글을 작성하다가 세션이 만료되었을 때 글을 작성할 수 없다는 경고문 띄우기  
+function check_input() {
+// https://johncom.tistory.com/19
+    var title = document.getElementById('title');
+
+    if (!title.value)
+    // 제목을 작성하지 않았다면
+    {
+        show_modal("제목을 입력해주세요!");
+        title.focus();
+        // 화면 커서 이동
+        return;
+    }
+    var contents = document.getElementById('contents');
+
+    if (!contents.value)
+    {
+        show_modal("내용을 입력해주세요!");
+        // 화면 커서 이동
+        contents.focus();
+        return;
+    }
+
+    document.getElementById("update_write_form").submit();
+    // 모두 확인 후 submit()
+ }
