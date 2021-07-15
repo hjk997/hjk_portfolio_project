3f3697f5769562c63e661e28d25e1135549a33ab
hjk997 <ununsk@gmail.com>
2021-07-16 02:23:35 +0900

    [#15] 단일 이미지 출력
    프로젝트 게시글을 확인했을 때, 하나의 이미지가 첨부되어있다면 확인할 수 있습니다.

--- src/main/java/com/hjkportfolio/hjk/image/ImageController.java
+++ src/main/java/com/hjkportfolio/hjk/image/ImageController.java
@@ -1,29 +1,33 @@
 package com.hjkportfolio.hjk.image;
 
-import org.apache.tomcat.jni.FileInfo;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.print.DocFlavor;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
+import java.io.OutputStream;
 
 @Controller
 public class ImageController {
 
     @GetMapping("images")
-    public String image(String path, String name) throws IOException {
+    public void image(String path, String name, HttpServletResponse response) throws IOException {
 
-        System.out.println("path : ");
+        // stream 처리
+        // 1. 경로 설정
+        StringBuilder sb = new StringBuilder();
+        String filePath = sb.append(new File("").getAbsolutePath()).append("\\").
+                append("images\\").append(path).append("\\").append(name).toString();
 
-        return "/";
+        // 2. 출력하기
+        OutputStream outputStream = response.getOutputStream();
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        FileCopyUtils.copy(fileInputStream, outputStream);
+
+        outputStream.flush();
 
     }
 }
